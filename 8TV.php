8030cfada5c67b776c99493f7648f4f174a854ba
AqFad <85995579+AqFad2811@users.noreply.github.com>
2021-10-01 10:54:22 +0800

    Create 8TV.php

---
+++ 8TV.php
@@ -0,0 +1,36 @@
+<?php
+
+$url = "https://www.dailymotion.com/player/metadata/video/kdFzSYy1bHxrForBrar?embedder=https%3A%2F%2Fwww.xtra.com.my%2Flive-tv&referer=&";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "Referer: https://www.xtra.com.my/live-tv",
+   "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+preg_match('/qualities":({.+?}),"/', $resp, $matches);
+$arr = json_decode($matches[1], true);
+    $m3u8_raw = $arr['auto'][0]['url'];
+    $url = $m3u8_raw;
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+echo $resp;
+?>
