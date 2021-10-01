3ca7f2f4f3f696516766882e7134f7bf2784206a
AqFad <85995579+AqFad2811@users.noreply.github.com>
2021-10-01 11:02:06 +0800

    Update download_token.sh

--- download_token.sh
+++ download_token.sh
@@ -4,31 +4,31 @@ echo -e "\e[1;31m [INFO] Create Folder \e[0m"
 mkdir m3u8
 
 echo -e "\e[1;31m [INFO] Downloading TV3 \e[0m"
-TV3="https://demo.samsam123.tk/mediaprima_dailymotion/TV3.php?"$AUTH
+TV3="https://raw.githubusercontent.com/AqFad2811/myiptv/main/TV3.php"$AUTH
 wget -O TV3.m3u8 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31" $TV3
 cp 'TV3.m3u8' 'stream/TV3.m3u8'
 rm 'TV3.m3u8'
 
 echo -e "\e[1;31m [INFO] Downloading DidikTVKPM \e[0m"
-DidikTVKPM="https://demo.samsam123.tk/mediaprima_dailymotion/didikTVKPM.php?"$AUTH
+DidikTVKPM="https://raw.githubusercontent.com/AqFad2811/myiptv/main/DidikTVKPM.php"$AUTH
 wget -O DidikTVKPM.m3u8 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31" $DidikTVKPM
 cp 'DidikTVKPM.m3u8' 'stream/DidikTVKPM.m3u8'
 rm 'DidikTVKPM.m3u8'
 
 echo -e "\e[1;31m [INFO] Downloading 8TV \e[0m"
-TV8="https://demo.samsam123.tk/mediaprima_dailymotion/8TV.php?"$AUTH
-wget -O 8TV.m3u8 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31" $TV8
+8TV="https://raw.githubusercontent.com/AqFad2811/myiptv/main/8TV.php"$AUTH
+wget -O 8TV.m3u8 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31" $8tv
 cp '8TV.m3u8' 'stream/8TV.m3u8'
 rm '8TV.m3u8'
 
 echo -e "\e[1;31m [INFO] Downloading Drama Sangat \e[0m"
-DramaSangat="https://demo.samsam123.tk/mediaprima_dailymotion/Drama_Sangat.php?"$AUTH
+DramaSangat="https://raw.githubusercontent.com/AqFad2811/myiptv/main/DramaSangat.php"$AUTH
 wget -O Drama_Sangat.m3u8 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31" $DramaSangat
 cp 'DramaSangat.m3u8' 'stream/DramaSangat.m3u8'
 rm 'DramaSangat.m3u8'
 
 echo -e "\e[1;31m [INFO] Downloading TV9 \e[0m"
-TV9="https://demo.samsam123.tk/mediaprima_dailymotion/TV9.php?"$AUTH
+TV9="https://raw.githubusercontent.com/AqFad2811/myiptv/main/TV9.php"$AUTH
 wget -O TV9.m3u8 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31" $TV9
 cp 'TV9.m3u8' 'stream/TV9.m3u8'
 rm 'TV9.m3u8'
